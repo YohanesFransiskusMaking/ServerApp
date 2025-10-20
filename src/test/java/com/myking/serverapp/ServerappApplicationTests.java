cc151f97bcbc9b22f114744a0451e66e4e2b8219
Myking <farymaking20@gmail.com>
2025-10-20 11:28:45 +0800

    menambahkan berbagai file untuk project serverApp

---
+++ src/test/java/com/myking/serverapp/ServerappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myking.serverapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
